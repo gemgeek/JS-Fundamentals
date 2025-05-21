49b9bc5457fd8e4620cce80f37796020ebf5ded0
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-05-21 14:44:36 +0000

    Fix Task 2: Match checker output with correct singular/plural format

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,8 @@
 const args = process.argv.slice(2);
 const count = args.length;
 
-if (args.length === 0) {
-  console.log("0 arguments");
-} else if (args.length === 1) {
-    console.log("1 Argument");
+if (count === 1) {
+  console.log("1 argument");
 } else {
-    console.log(`${count} arguments`);
+  console.log(`${count} arguments`);
 }
